d32003684a4d23dc7bb12be1f739d5e84c60f8fc
Gerardo Daniel Muela <gerardomuela96@gmail.com>
2018-03-21 18:50:41 -0600

    Made changes to labels in NewProjectWindow and WorkspaceLauncher

--- NewProjectWindow.py
+++ NewProjectWindow.py
@@ -3,8 +3,8 @@ import gtk, gobject, os
 class NewProjectWindow(gtk.Window):
 
   def __init__(self):
-  
-    #Export Window 
+
+    #Export Window
     super(NewProjectWindow, self).__init__()
     self.set_title("New Project")
     self.set_default_size(900,900)
@@ -13,24 +13,24 @@ class NewProjectWindow(gtk.Window):
 
     # Create table
     table = gtk.Table(rows=6, columns=10, homogeneous=False)
-    
+
     # Create labels
     lblM = gtk.Label()
     lblM.set_markup("\n<b>Create a new project</b>")
     lblM.set_justify(gtk.JUSTIFY_CENTER)
     lbl1 = gtk.Label("Project Name")
     lbl2 = gtk.Label("Description")
-    
+
     # Create textViews
     txtBox1 = gtk.TextView()
     txtBox1.set_editable(False)
     txtBox2 = gtk.TextView()
     txtBox2.set_editable(False)
-    
+
     # Create buttons
-    CreateBtn = gtk.Button("CREATE")
-    CancelBtn = gtk.Button("CANCEL")
-    
+    CreateBtn = gtk.Button("Create")
+    CancelBtn = gtk.Button("Cancel")
+
     # Attaach everithing to the table
     table.attach(lblM,0,3,0,1)
     table.attach(lbl1,0,1,1,2)
@@ -39,13 +39,13 @@ class NewProjectWindow(gtk.Window):
     table.attach(txtBox2,1,9,2,5)
     table.attach(CreateBtn,8,9,5,6)
     table.attach(CancelBtn,9,10,5,6)
-    
+
     table.set_row_spacings(20)
     table.set_col_spacings(15)
     # Method for closing window
     def exit(widget,callback_data = None):
         self.destroy()
-    
+
     # set the action of the button
     CancelBtn.connect("clicked", exit)
 
@@ -53,6 +53,6 @@ class NewProjectWindow(gtk.Window):
     self.set_default_size(0,0)
     self.set_resizable(False)
     self.show_all()
-    
+
 #NewProjectWindow()
 #gtk.main()
--- WorkspaceLauncherWindow.py
+++ WorkspaceLauncherWindow.py
@@ -3,26 +3,26 @@ import gtk, gobject, os
 class WorkspaceLauncherWindow(gtk.Window):
 
   def __init__(self):
-  
-    #Export Window 
+
+    #Export Window
     super(WorkspaceLauncherWindow, self).__init__()
     self.set_title("Workspace Launcher")
     self.set_default_size(450,600)
     self.set_position(gtk.WIN_POS_CENTER)
     self.set_border_width(10)
-    
+
     # Create labels and buttons
     table = gtk.Table(rows=3, columns=3, homogeneous=False)
     lblM = gtk.Label()
     lblM.set_markup("\n<b>Select a directory as a workspace: PDGS uses the workspace\ndirectory to store projects</b>")
     lblM.set_justify(gtk.JUSTIFY_CENTER)
-    lbl1 = gtk.Label("WORKSPACE")
+    lbl1 = gtk.Label("Workspace")
     txtBox1 = gtk.TextView()
     txtBox1.set_editable(False)
-    btn1 = gtk.Button("BROWSE")
-    btn3 = gtk.Button("LAUNCH")
-    btn4 = gtk.Button("CANCEL")
-    
+    btn1 = gtk.Button("Browse")
+    btn3 = gtk.Button("Launch")
+    btn4 = gtk.Button("Cancel")
+
     # Attach all the buttons and labels to the table
     table.attach(lblM,0,4,0,1)
     table.attach(lbl1,0,1,1,2)
@@ -31,10 +31,10 @@ class WorkspaceLauncherWindow(gtk.Window):
     table.attach(btn1,2,3,1,2)
     table.attach(btn3,1,2,2,3)
     table.attach(btn4,2,3,2,3)
-    
+
     table.set_row_spacings(20)
     table.set_col_spacings(15)
-    
+
     # Method for canceling
     def cancel(widget,callback_data = None):
         self.destroy()
@@ -48,7 +48,7 @@ class WorkspaceLauncherWindow(gtk.Window):
             textBuffer.set_text(dialog.get_filename())
             textview.set_buffer(textBuffer)
         dialog.destroy()
-        
+
     # set the action of the button
     btn1.connect('clicked',chooseFile,txtBox1)
     btn4.connect('clicked',cancel)
